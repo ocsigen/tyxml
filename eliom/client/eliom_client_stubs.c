e6a2a6240dcb956b816bb97b541f3056e47c13cf
gregoire.henry <gregoire.henry@pps.jussieu.fr>
2011-03-12 14:43:38 +0100

    SPLIT PACKAGE: Removing ocsigenserver and eliom files.
    Ignore-this: 3d521120218aec26432640cf3ec2ed76
    
    darcs-hash:20110312134338-da4f2-0e9b9b1f1cdca2c62f0dbf3e8408a51192531897

--- eliom/client/eliom_client_stubs.c
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#define D(f) void f () { exit(1); }
-D(caml_register_closure)
-D(caml_regexp_make)
-D(caml_regexp_split)
-D(caml_get_obj_table_uuid)
--- eliom/client/eliom_client_unwrap.mli
+++
@@ -1,8 +0,0 @@
-
-type unwrap_id
-
-val register_unwrapper : unwrap_id -> ('a -> 'b) -> unit
-
-val unwrap : ('b*'a) -> 'a
-
-val id_of_int : int -> unwrap_id
