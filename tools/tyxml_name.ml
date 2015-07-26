236e0845dab9f5a4661fa3a3674361624245bddd
Drup <drupyog@zoho.com>
2015-07-26 21:33:58 +0200

    Rewrite to work on 3.12.

--- tools/tyxml_name.ml
+++ tools/tyxml_name.ml
@@ -15,28 +15,32 @@ let is_identchar = function
   | '0'..'9' -> true
   | _ -> false
 
-let to_ocaml s =
+let to_ocaml_bytes s =
   let f c = if is_identchar c then c else '_' in
-  String.map f s
+  Bytes.init (String.length s) (fun i -> f s.[i])
+
+let to_ocaml s =
+  Bytes.to_string (to_ocaml_bytes s)
 
 
 let mapi f s =
   let l = String.length s in
-  String.init l (fun i -> f i (String.get s i))
+  Bytes.init l (fun i -> f i (String.get s i))
 
 
 let ident s =
-  let s = to_ocaml s in
-  mapi (fun i c ->
+  let s = to_ocaml_bytes s in
+  let s = Bytes.mapi (fun i c ->
     if i = 0 then Char.lowercase c else c)
-    s
+    s in
+  Bytes.to_string s
 
 let attrib s =
-  "a_" ^ to_ocaml s
+  "a_" ^  to_ocaml s
 
 let polyvar s =
-  let s = to_ocaml s in
-  let s = mapi (fun i c ->
+  let s = to_ocaml_bytes s in
+  let s = Bytes.mapi (fun i c ->
     if i = 0 then Char.uppercase c else c)
     s in
-  "`" ^ s
+  "`" ^ Bytes.to_string s
