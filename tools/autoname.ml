b5f8008fc4bb2aa61e0bac885bdd49016b9c72ce
Drup <drupyog@zoho.com>
2015-07-26 20:05:17 +0200

    Add a small tool to give the correc tyxml name.

---
+++ tools/autoname.ml
@@ -0,0 +1,21 @@
+
+let arg = ref false
+
+let answer_elem ~attrib s =
+  Printf.printf
+    "%s\t%s\t%s\n%!"
+    s
+    (if attrib then Tyxml_name.attrib s else Tyxml_name.ident s)
+    (Tyxml_name.polyvar s)
+
+let spec = [
+  "-a", Arg.String (answer_elem ~attrib:true), "Returns the tyxml names for the given attribute." ;
+  "-e", Arg.String (answer_elem ~attrib:false), "Returns the tyxml names for the given element." ;
+
+]
+
+let usage =
+  "Accepts HTML attributes and elements and returns their tyxml names.
+Names without option are treated as elements."
+
+let () = Arg.parse spec (answer_elem ~attrib:false) usage
---
+++ tools/tyxml_name.ml
@@ -0,0 +1,36 @@
+
+(* Elements and attributes are technically utf8, but ascii is enough for now.
+
+   see <http://www.w3.org/TR/html51/syntax.html#syntax>
+*)
+
+(* In the ocaml parser:
+   let identchar = ['A'-'Z' 'a'-'z' '_' '\'' '0'-'9']
+*)
+let is_identchar = function
+  | 'A'..'Z'
+  | 'a'..'z'
+  | '_'
+  | '\''
+  | '0'..'9' -> true
+  | _ -> false
+
+let to_ocaml s =
+  let f c = if is_identchar c then c else '_' in
+  String.map f s
+
+let ident s =
+  let s = to_ocaml s in
+  String.mapi (fun i c ->
+    if i = 0 then Char.lowercase c else c)
+    s
+
+let attrib s =
+  "a_" ^ to_ocaml s
+
+let polyvar s =
+  let s = to_ocaml s in
+  let s = String.mapi (fun i c ->
+    if i = 0 then Char.uppercase c else c)
+    s in
+  "`" ^ s
