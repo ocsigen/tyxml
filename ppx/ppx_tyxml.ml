7ce2be0298947fa4360f613cc9eb24610b5217c9
Drup <drupyog@zoho.com>
2016-03-30 03:47:05 +0200

    Implement antiquotations using placeholder elements.

--- ppx/ppx_tyxml.ml
+++ ppx/ppx_tyxml.ml
@@ -20,7 +20,94 @@
 open Asttypes
 open Parsetree
 
+(** Antiquotations
 
+    We replace antiquotations expressions by a dummy token "(tyxmlX)".
+    We store a table token to expression to retrieve them after parsing.
+*)
+module Antiquot = struct
+
+  let fmt_id = Printf.sprintf "(tyxml%i)"
+  let regex_id = Re.(seq [ str "(tyxml" ; rep digit ; char ')' ])
+  let re_id = Re.compile regex_id
+  let whole_re_id = Re.(compile @@ whole_string regex_id)
+
+  let make_id =
+    let r = ref 0 in
+    fun () -> incr r ; fmt_id !r
+
+  module H = Hashtbl.Make(struct
+      type t = string
+      let hash = Hashtbl.hash
+      let equal (x:string) y = x = y
+    end)
+
+  let tbl = H.create 17
+
+  let create expr =
+    let s = make_id () in
+    H.add tbl s expr ;
+    s
+
+  let get loc s =
+    if H.mem tbl s then H.find tbl s
+    else
+      Ppx_common.error loc
+        "Internal error: This expression placeholder is not registered."
+
+  let mem s = H.mem tbl s
+
+  let contains loc s = match Re.exec_opt re_id s with
+    | None -> `No
+    | Some g ->
+      let (i,j) = Re.Group.offset g 0 in
+      let is_whole = i = 0 && j = String.length s in
+      if is_whole
+      then `Whole (get loc s)
+      else `Yes (get loc @@ Re.Group.get g 0)
+
+  let assert_no_antiquot ~loc kind (_namespace,s) =
+    match contains loc s with
+    | `No -> ()
+    | `Yes e | `Whole e ->
+      Ppx_common.error e.pexp_loc
+        "OCaml expressions are not accepted as %s names." kind
+
+end
+
+(** Building block to rebuild the output with expressions intertwined. *)
+
+let make_pcdata ~loc s =
+  [%expr pcdata [%e Ppx_common.string loc s]][@metaloc loc]
+
+(** Walk the text list to replace placeholders by OCaml expressions when
+    appropriate. Use {!make_pcdata} on the rest. *)
+let make_text ~loc ss =
+  let buf = Buffer.create 17 in
+  let push_pcdata buf l =
+    let s = Buffer.contents buf in
+    Buffer.clear buf ;
+    if s = "" then l else make_pcdata ~loc s :: l
+  in
+  let rec aux ~loc res = function
+    | [] -> push_pcdata buf res
+    | `Text s :: t ->
+        Buffer.add_string buf s ;
+        aux ~loc res t
+    | `Delim g :: t ->
+      let e = Antiquot.get loc @@ Re.get g 0 in
+      aux ~loc (e :: push_pcdata buf res) t
+  in
+  aux ~loc [] @@ Re.split_full Antiquot.re_id @@ String.concat "" ss
+
+let replace_attribute ~loc (attr,value) =
+  Antiquot.assert_no_antiquot ~loc "attribute" attr ;
+  match Antiquot.contains loc value with
+  | `No -> (attr, `String value)
+  | `Whole e -> (attr, `Expr e)
+  | `Yes _ ->
+      Ppx_common.error loc
+      "Mixing literals and OCaml expressions is not authorized in attribute values."
 
 (* Converts a Markup.ml input location into an OCaml location. [start_loc] is
    the OCaml location of the string being parsed by Markup.ml.
@@ -61,19 +148,10 @@ let adjust_location start_loc delimiter_length consumed (line, column) =
    [markup_to_expr] first converts the payload to a list of strings and TyXML
    expressions. It then builds an input stream for Markup.ml, which walks this
    list. Bytes in strings encountered are passed to Markup.ml. When a TyXML
-   expression is encountered, it is appended to the current child list.
-
-   The current child list is a piece of state maintained by the assembler, which
-   reads the Markup.ml signal (output) stream and recursively assembles the
-   TyXML expression.
-
-   The current implementation stores the child list in a reference, because it
-   is modified by both the assembler and the input stream function. A better
-   implementation would scan the payload for the locations of literal TyXML
-   expressions, and merge them into the child list in the assembler. *)
+   expression is encountered, a dummy token is inserted that is later replaced by
+   the proper expression. *)
 let markup_to_expr loc expr =
   let current_adjust_location = ref (adjust_location Location.none 0 0) in
-  let current_children = ref [] in
 
   let input_stream =
     let expressions =
@@ -83,28 +161,28 @@ let markup_to_expr loc expr =
     in
 
     let strings_and_antiquotations =
-      expressions |> List.map (fun expr ->
-        match expr.pexp_desc with
-        (* TODO: Doesn't work in 4.03, can't pattern match. *)
-        | Pexp_constant (Const_string (s, maybe_delimiter)) ->
-          let delimiter_length =
-            match maybe_delimiter with
-            | None -> 1
-            | Some d -> String.length d + 2
-          in
-          `String (s, expr.pexp_loc, delimiter_length)
+      expressions |> List.map @@ fun expr ->
+      match expr.pexp_desc with
+      (* TODO: Doesn't work in 4.03, can't pattern match. *)
+      | Pexp_constant (Const_string (s, maybe_delimiter)) ->
+        let delimiter_length =
+          match maybe_delimiter with
+          | None -> 1
+          | Some d -> String.length d + 2
+        in
+        (s, expr.pexp_loc, delimiter_length)
 
-        | _ ->
-          `Expression expr)
+      | _ ->
+        (Antiquot.create expr, expr.pexp_loc, 0)
     in
 
     let items = ref strings_and_antiquotations in
     let offset = ref 0 in
     let consumed = ref 0 in
 
-    let rec next () =
-      match !items with
-      | (`String (s, loc, delimiter_length))::rest ->
+    let rec next () = match !items with
+      | [] -> None
+      | (s, loc, delimiter_length)::rest ->
         if !offset = 0 then begin
           current_adjust_location :=
             adjust_location loc delimiter_length !consumed;
@@ -120,14 +198,6 @@ let markup_to_expr loc expr =
           items := rest;
           next ()
         end
-
-      | (`Expression expr)::rest ->
-        current_children := expr::!current_children;
-        items := rest;
-        next ()
-
-      | [] ->
-        None
     in
 
     Markup.fn next
@@ -150,39 +220,33 @@ let markup_to_expr loc expr =
   in
   let signals = Markup.signals parser in
 
-  let rec assemble () =
-    match Markup.next signals with
-    | None | Some `End_element ->
-      current_children := List.rev !current_children
-
-    | Some (`Text ss) ->
-      let loc = parser |> Markup.location |> !current_adjust_location in
-      let node =
-        [%expr pcdata [%e Ppx_common.string loc (String.concat "" ss)]]
-          [@metaloc loc]
-      in
-      current_children := node::!current_children;
-      assemble ()
-
-    | Some (`Start_element (name, attributes)) ->
-      let loc = parser |> Markup.location |> !current_adjust_location in
-
-      let accumulator = !current_children in
-      current_children := [];
-      assemble ();
-      let children = !current_children in
-
-      let node = Ppx_element.parse ~loc ~name ~attributes children in
-
-      current_children := node::accumulator;
-      assemble ()
-
-    | Some _ ->
-      assemble ()
+  let get_loc () =
+    parser |> Markup.location |> !current_adjust_location
   in
 
-  assemble ();
-  Ppx_common.list loc !current_children
+  let rec assemble children =
+    match Markup.next signals with
+    | None | Some `End_element -> List.rev children
+
+    | Some (`Text ss) ->
+      let loc = get_loc () in
+      let node = make_text ~loc ss in
+      assemble (node @ children)
+
+    | Some (`Start_element (name, attributes)) ->
+      let loc = get_loc () in
+
+      let sub_children = assemble [] in
+      Antiquot.assert_no_antiquot ~loc "element" name ;
+      let attributes = List.map (replace_attribute ~loc) attributes in
+      let node = Ppx_element.parse ~loc ~name ~attributes sub_children in
+      assemble (node :: children)
+
+    | Some _ ->
+      assemble children
+  in
+
+  Ppx_common.list loc @@ assemble []
 
 
 
