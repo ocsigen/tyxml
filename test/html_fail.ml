3e69bb24bbfb3902775604acab8f1d751287a4dd
Drup <drupyog@zoho.com>
2018-04-11 18:18:41 +0200

    Add back the toplevel test.

--- test/html_fail.ml
+++ test/html_fail.ml
@@ -1,11 +1,3 @@
-#use "topfind" ;;
-#require "uutf" ;;
-#require "re" ;;
-#directory "lib" ;;
-#directory "implem" ;;
-#load "tyxml_f.cma" ;;
-#load "tyxml.cma" ;;
-
 open Tyxml.Html ;;
 
 div [a [a []]] ;;
