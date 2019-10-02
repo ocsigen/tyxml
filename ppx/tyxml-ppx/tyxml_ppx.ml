bf34a64d821a536caa1697a56cef7092fc72fe4e
Joris Giovannangeli <joris.giovannangeli@ahrefs.com>
2019-10-02 09:24:32 +0800

    ppx: move everything but the ast mapper to a lib

--- ppx/tyxml-ppx/tyxml_ppx.ml
+++ ppx/tyxml-ppx/tyxml_ppx.ml
@@ -17,6 +17,8 @@
  * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1301, USA.
 *)
 
+open Tyxml_ppx_common
+
 (* When dropping support for 4.02, this module can simply be deleted. *)
 module String = struct
   include String
