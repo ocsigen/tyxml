f8aa57beb78b04f4953a68a8a9f467efca9e280e
Vincent Balat <vincent.balat@vblt.org>
2026-08-27 16:45:58 +0200

    Fix the dead manual reference in the Tyxml module documentation
    The intro page became index.mld when the manual moved to odoc, and the raw
    markup wrapper hid the dead reference from odoc, so the link rendered as
    plain text instead of being reported.

--- implem/tyxml.ml
+++ implem/tyxml.ml
@@ -2,8 +2,8 @@
 
     This is the natural implementation of the TyXML combinators
     based on an XML data-structure.
-    {%
-    Other implementations are available, see {{!page-"intro"}the manual} for details. %}
+    Other implementations are available, see {{!page-"index"}the manual} for
+    details.
 *)
 
 (** Typesafe constructors and printers for HTML documents.
